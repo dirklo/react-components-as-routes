495a4bd83e1e2e784cbcbbe03550b245aa6eba89
Rick <dirklo@gmail.com>
2021-04-17 15:19:10 +0200

    commit tree d0c0fbeaaca712c1894d208b2ef4aa27f488f68f

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Login from './components/Login'
 
 ReactDOM.render(
-  <Home />,
+  <Router>
+    <div>
+      <Navbar />
+      <Route path="/" render={() => <Home />}></Route>
+      <Route exact path="/about" render={() => <About />}></Route>
+      <Route exact path="/login" render={() => <Login />}></Route>
+    </div>
+  </Router>,
   document.getElementById('root')
 );
